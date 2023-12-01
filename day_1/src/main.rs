026156c601ab85bdcae3cb0f3eb0ab7a63ec745f
Tomas Rinaldi <tomas.rinaldi@globant.com>
2023-12-01 15:56:12 -0300

    Day 1 - Part 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,38 +1,72 @@
 use std::error::Error;
 use std::io::{stdin, BufRead};
-use tokio;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+#[derive(Debug)]
+struct Digits(usize, usize);
+
+impl Digits {
+    fn append_digits(digits: Option<Digits>, digit: usize) -> Option<Digits> {
+        if let Some(existing_digits) = digits {
+            Some(Digits(existing_digits.0, digit))
+        } else {
+            Some(Digits(digit, digit))
+        }
+    }
+
+    fn to_number(&self) -> usize {
+        (self.0 * 10 + self.1).into()
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let mut sum = 0;
     for line_result in stdin().lock().lines() {
         let line = line_result?;
-        let number = extract_number(&line);
-        // println!("{} -> {}", line, number);
-        sum += number;
+        let digits = extract_digits(&line);
+        
+        if let Some(found_digits) = digits {
+            let number = found_digits.to_number();
+            // println!("{} -> {:?} -> {}", line, found_digits, number);
+            sum += number;
+        } else {
+            println!("No digits found in line: {}", line);
+        }
     }
     println!("{}", sum);
     Ok(())
 }
 
-fn extract_number(s: &str) -> usize {
-    let mut digits: Option<[char; 2]> = None;
-    for ch in s.chars() {
-        if !ch.is_numeric() {
-            continue;
-        }
-
-        if let Some(existing_nums) = digits {
-            digits = Some([existing_nums[0], ch]);
-        } else {
-            digits = Some([ch, ch]);
+fn extract_digits(s: &str) -> Option<Digits> {
+    let mut digits: Option<Digits> = None;
+    for (i, ch) in s.chars().enumerate() {
+        if ch.is_numeric() {
+            let digit = ch as usize - '0' as usize;
+            digits = Digits::append_digits(digits, digit);
+        } else if let Some(digit) = try_get_number_name(&s[i..]) {
+            digits = Digits::append_digits(digits, digit)
         }
     }
+    digits
+}
 
-    if let Some(found_digits) = digits {
-        return String::from_iter(found_digits).parse::<usize>().unwrap();
-    } else {
-        println!("Digits not found in string: {}", s);
-        return 0;
+fn try_get_number_name(s: &str) -> Option<usize> {
+    const NUMBER_NAMES: [&str; 10] = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+
+    for (i, name) in NUMBER_NAMES.iter().enumerate() {
+        if s.starts_with(name) {
+            return Some(i);
+        }
     }
+    return None;
 }
