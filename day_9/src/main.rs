38776435498c15c0da8dd25a3f1b711fbfe3dd09
Tomás Rinaldi <tomi901@users.noreply.github.com>
2023-12-09 12:16:13 -0300

    Day 9 - Part 2

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -1,24 +1,51 @@
-use std::io::{stdin, BufRead};
+use std::io::stdin;
 
 fn main() {
-    let mut sum = 0;
+    let mut previous_sum = 0;
+    let mut next_sum = 0;
     for line_result in stdin().lines() {
         let line = line_result.unwrap();
         let nums: Vec<_> = line
             .split(' ')
             .map(|s| s.parse::<isize>().unwrap())
             .collect();
-        let next = predict_next_number(&nums);
-        sum += next;
+        previous_sum += predict_previous_number(&nums);
+        next_sum += predict_next_number(&nums);
     }
-    println!("{sum}");
+    println!("Previous sum: {previous_sum}");
+    println!("Next sum: {next_sum}");
 }
 
 fn predict_next_number(nums: &[isize]) -> isize {
+    let diff_result = get_diffs(&nums);
+    
+    let next_expected_diff = if diff_result.all_equal {
+        *diff_result.diffs.first().unwrap()
+    } else {
+        predict_next_number(&diff_result.diffs)
+    };
+    
+    // println!("{:?} -> {:?} = {}", &nums, &diffs, next_expected_diff);
+    nums.last().unwrap() + next_expected_diff
+}
+
+fn predict_previous_number(nums: &[isize]) -> isize {
+    let diff_result = get_diffs(&nums);
+
+    let next_expected_diff = if diff_result.all_equal {
+        *diff_result.diffs.first().unwrap()
+    } else {
+        predict_previous_number(&diff_result.diffs)
+    };
+
+    // println!("{:?} -> {:?} = {}", &nums, &diffs, next_expected_diff);
+    nums.first().unwrap() - next_expected_diff
+}
+
+fn get_diffs(nums: &[isize]) -> DiffResult {
     let mut next_iter = nums.into_iter();
     next_iter.next().expect("Empty slice.");
-    
-    
+
     let mut diffs = Vec::with_capacity(nums.len() - 1);
     let mut all_equal = true;
     let mut first_diff = None;
@@ -29,19 +56,16 @@ fn predict_next_number(nums: &[isize]) -> isize {
             first_diff = Some(diff);
             continue;
         }
-        
+
         if diff != first_diff.unwrap() {
             all_equal = false;
         }
     }
     
-    
-    let next_expected_diff = if all_equal {
-        first_diff.unwrap()
-    } else {
-        predict_next_number(&diffs)
-    };
-    
-    // println!("{:?} -> {:?} = {}", &nums, &diffs, next_expected_diff);
-    nums.last().unwrap() + next_expected_diff
+    DiffResult { diffs, all_equal }
+}
+
+struct DiffResult {
+    diffs: Vec<isize>,
+    all_equal: bool,
 }
