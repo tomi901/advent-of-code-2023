c18588dadc2a9d51a2d3cc75ca20b86ed223d8eb
Tomás Rinaldi <tomi901@users.noreply.github.com>
2023-12-14 20:49:34 -0300

    Day 14 - Part 1

--- day_14/src/main.rs
+++ day_14/src/main.rs
@@ -1,14 +1,23 @@
+use std::fmt::{Display, Formatter};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
     let path = std::env::current_dir().unwrap().join("day_14/input.txt");
-    println!("Opening file {}", path.display());
+    println!("Opening file: {}", path.display());
+    println!();
     let file = File::open(path).unwrap();
     let mut reader = BufReader::new(file);
 
-    let map = Map::try_from(&mut reader).unwrap();
-    println!("{}x{}", map.width, map.height);
+    let mut map = Map::try_from(&mut reader).unwrap();
+    println!("Starting map:");
+    println!("{}", &map);
+    println!();
+    
+    map.tilt_north();
+    println!("Tilted map:");
+    println!("{}", &map);
+    println!();
 
     let load = map.calculate_north_load();
     println!("Load: {}", load);
@@ -17,12 +26,12 @@ fn main() {
 type Tile = char;
 const ROUND_ROCK: Tile = 'O';
 const CUBE_ROCK: Tile = '#';
-const SPACE: Tile = '.';
+const GROUND: Tile = '.';
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, Default)]
 struct Point2D(usize, usize);
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Direction {
     North,
     East,
@@ -30,10 +39,24 @@ enum Direction {
     West,
 }
 
+#[derive(Clone)]
 struct Map {
     tiles: Vec<Tile>,
     width: usize,
     height: usize,
+}
+
+impl Display for Map {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for row_i in 0..self.height {
+            let start_i = row_i * self.width;
+            for tile in &self.tiles[start_i..(start_i + self.width)] {
+                write!(f, "{}", tile)?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
 }
 
 impl Map {
@@ -43,13 +66,17 @@ impl Map {
             Some(line) => line,
             None => return None,
         };
-
-        let mut tiles: Vec<_> = first_line.chars().collect();
+        let mut tiles: Vec<_> = first_line.chars().skip(1).collect();
         let width = tiles.len();
+        let mut expected_length = width;
+
         let mut height = 1;
         for line in lines {
             tiles.extend(line.chars());
             height += 1;
+
+            expected_length += width;
+            assert_eq!(tiles.len(), expected_length);
         }
         Some(Self {
             tiles,
@@ -86,5 +113,62 @@ impl Map {
             }
         }
         load
+    }
+
+    fn tilt(&mut self, direction: Direction) {
+        for x in 0..self.width {
+            let mut cur_sequence = 0;
+            for y in (0..self.height).rev() {
+                let point = Point2D(x, y);
+                let index = self.get_index(point).expect("Out of range");
+                match self.tiles[index] {
+                    CUBE_ROCK if (y + 1) < self.height => {
+                        let from = y + 1;
+                        for previous_y in from..(from + cur_sequence) {
+                            let index = self.get_index(Point2D(x, previous_y)).unwrap();
+                            self.tiles[index] = ROUND_ROCK;
+                        }
+                        cur_sequence = 0;
+                    },
+                    ROUND_ROCK => {
+                        self.tiles[index] = GROUND;
+                        cur_sequence += 1;
+                    },
+                    _ => {}
+                }
+            }
+
+            if cur_sequence > 0 {
+                for previous_y in 0..cur_sequence {
+                    let index = self.get_index(Point2D(x, previous_y)).unwrap();
+                    self.tiles[index] = ROUND_ROCK;
+                }
+            }
+        }
+    }
+}
+
+struct ScanLine<'a> {
+    map: &'a Map,
+    number: usize,
+    direction: Direction,
+}
+
+impl ScanLine {
+    /// Gets the point at the scan line index
+    fn get_point(&self, i: usize) -> Point2D {
+        match self.direction {
+            Direction::North => Point2D(self.number, self.map.height - i),
+            Direction::East => Point2D(i, self.number),
+            Direction::South => Point2D(self.number, i),
+            Direction::West => Point2D(self.map.width - i, self.number),
+        }
+    }
+    
+    fn len(&self) -> usize {
+        match self.direction {
+            Direction::North | Direction::South => self.map.height,
+            Direction::East | Direction::West => self.map.width,
+        }
     }
 }
